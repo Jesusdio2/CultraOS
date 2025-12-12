abfc886b2a23cd81800e700f36c1c4e8dee8d741
Francisco <frankwither991000@gmail.com>
2025-12-12 13:33:59 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@ set -e
 
 CC="gcc"
 LD="ld"
-CFLAGS="-m32 -ffreestanding -O2 -Wall -Wextra -nostdlib"
+CFLAGS="-m32 -ffreestanding -O2 -Wall -Wextra -nostdlib -Isrc/drivers -Isrc/kernel"
 LDFLAGS="-T src/kernel/linker.ld"
 
 # Compilar fuentes del kernel
 $CC $CFLAGS -c src/kernel/kmain.c -o src/kernel/kmain.o
 $CC $CFLAGS -c src/kernel/kernel.c -o src/kernel/kernel.o
+$CC $CFLAGS -c src/kernel/console.c -o src/kernel/console.o   # <-- agregado
 
 # Compilar drivers
 $CC $CFLAGS -c src/drivers/fb.c -o src/drivers/fb.o
@@ -24,5 +25,5 @@ mkdir -p src/iso/boot/grub
 cp src/kernel.bin src/iso/boot/kernel.bin
 echo 'set timeout=0' > src/iso/boot/grub/grub.cfg
 echo 'set default=0' >> src/iso/boot/grub/grub.cfg
-echo 'menuentry "MiOS" { multiboot /boot/kernel.bin }' >> src/iso/boot/grub/grub.cfg
+echo 'menuentry "CultraCore" { multiboot /boot/kernel.bin }' >> src/iso/boot/grub/grub.cfg
 grub-mkrescue -o src/os.iso src/iso
