a716c76660a360fcce94f2037f217d9d9a5de22c
Francisco <frankwither991000@gmail.com>
2025-12-12 13:21:29 -0700

    Delete src/drivers/keyboard.c

--- src/drivers/keyboard.c
+++
@@ -1,8 +0,0 @@
-// src/drivers/keyboard.c
-#include "keyboard.h"
-#include "io.h"   // para inb/outb si ya lo tienes
-
-void keyboard_init(void) {
-    // Inicialización mínima del teclado (PS/2)
-    // Por ahora puede estar vacío
-}
