6ad22dd19e8eb93f72c396b82309bde9a1bde8f2
Francisco <frankwither991000@gmail.com>
2025-12-12 14:50:30 -0700

    Update framebuffer.h

--- src/drivers/framebuffer.h
+++ src/drivers/framebuffer.h
@@ -1,14 +1,12 @@
 #pragma once
 #include <stdint.h>
-#include "multiboot2.h"
+#include "../kernel/multiboot2.h"   // ruta correcta según tu proyecto
 
-// Variables globales del framebuffer
 extern uint32_t fb_width;
 extern uint32_t fb_height;
 extern uint32_t fb_pitch;
 extern void *framebuffer;
 
-// Funciones del framebuffer
 void fb_init(struct multiboot_tag_framebuffer *tag);
 void fb_putpixel(int x, int y, uint32_t color);
 void fb_clear(uint32_t color);
