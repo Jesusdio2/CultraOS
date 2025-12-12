f593a5566f11aea99b1139d20be76a5c2d7d9e21
Francisco <frankwither991000@gmail.com>
2025-12-12 13:51:57 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,17 @@ CFLAGS="-m64 -ffreestanding -O2 -Wall -Wextra -nostdlib -fno-pic -fno-pie -Isrc/
 LDFLAGS="-T src/kernel/linker.ld"
 
 # Compilar fuentes del kernel
-$CC $CFLAGS -c src/kernel/kmain.c   -o src/kernel/kmain.o
-$CC $CFLAGS -c src/kernel/console.c -o src/kernel/console.o
-$CC $CFLAGS -c src/kernel/string.c  -o src/kernel/string.o
-$CC $CFLAGS -c src/kernel/idt.c     -o src/kernel/idt.o
+$CC $CFLAGS -c src/kernel/kmain.c      -o src/kernel/kmain.o
+$CC $CFLAGS -c src/kernel/console.c    -o src/kernel/console.o
+$CC $CFLAGS -c src/kernel/string.c     -o src/kernel/string.o
+$CC $CFLAGS -c src/kernel/idt.c        -o src/kernel/idt.o
+$CC $CFLAGS -c src/kernel/multiboot2.c -o src/kernel/multiboot2.o   # <-- agregado
 
 # Compilar drivers
-$CC $CFLAGS -c src/drivers/fb.c     -o src/drivers/fb.o
-$CC $CFLAGS -c src/drivers/cursor.c -o src/drivers/cursor.o
-$CC $CFLAGS -c src/drivers/mouse.c  -o src/drivers/mouse.o
-$CC $CFLAGS -c src/drivers/time.c   -o src/drivers/time.o
+$CC $CFLAGS -c src/drivers/fb.c        -o src/drivers/fb.o
+$CC $CFLAGS -c src/drivers/cursor.c    -o src/drivers/cursor.o
+$CC $CFLAGS -c src/drivers/mouse.c     -o src/drivers/mouse.o
+$CC $CFLAGS -c src/drivers/time.c      -o src/drivers/time.o
 
 # Enlazar kernel
 $LD $LDFLAGS -o src/kernel.bin src/kernel/*.o src/drivers/*.o
