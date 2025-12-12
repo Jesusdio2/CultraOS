7e2897857097b9bb5ce348e2589fc5344a1209c0
Francisco <frankwither991000@gmail.com>
2025-12-12 14:48:28 -0700

    Update kmain.c

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -1,13 +1,15 @@
 #include <stdint.h>
 #include "io.h"
 #include "idt.h"
-#include "../drivers/mouse.h"
-#include "../drivers/framebuffer.h"  // tu nuevo driver
 #include "console.h"
 #include "multiboot2.h"
 
+#include "../drivers/framebuffer.h"
+#include "../drivers/mouse.h"
+#include "../drivers/cursor.h"
+
 void kmain(uint32_t magic, uint32_t addr) {
-    // Inicializar IDT y controladores
+    // Inicializar IDT y controladores básicos
     idt_init();
     mouse_init();
 
@@ -24,11 +26,15 @@ void kmain(uint32_t magic, uint32_t addr) {
             if (tag->type == MULTIBOOT_TAG_TYPE_FRAMEBUFFER) {
                 fb_init((struct multiboot_tag_framebuffer*)tag);
                 fb_clear(0x000000); // negro
+
+                // Inicializar y dibujar cursor en el centro
+                cursor_init(fb_width/2, fb_height/2);
+                cursor_draw();
             }
         }
     }
 
-    // También puedes escribir directamente en memoria VGA si quieres
+    // También puedes escribir directamente en memoria VGA como prueba
     const char *msg = "Hola desde mi kernel en C!\n";
     volatile char *video = (volatile char*)0xB8000;
     for (int i = 0; msg[i] != 0; i++) {
@@ -36,8 +42,8 @@ void kmain(uint32_t magic, uint32_t addr) {
         video[i*2+1] = 0x07; // gris sobre negro
     }
 
-    // Loop principal
+    // Loop principal: espera interrupciones
     while (1) {
-        asm volatile("hlt"); // espera interrupciones
+        asm volatile("hlt");
     }
 }
