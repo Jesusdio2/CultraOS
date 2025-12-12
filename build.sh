cf5a60d7629de07652208fbbe43c7eb2dfbc8f14
Francisco <frankwither991000@gmail.com>
2025-12-12 13:38:31 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,15 +7,16 @@ CFLAGS="-m32 -ffreestanding -O2 -Wall -Wextra -nostdlib -Isrc/drivers -Isrc/kern
 LDFLAGS="-T src/kernel/linker.ld"
 
 # Compilar fuentes del kernel
-$CC $CFLAGS -c src/kernel/kmain.c -o src/kernel/kmain.o
-$CC $CFLAGS -c src/kernel/kernel.c -o src/kernel/kernel.o
-$CC $CFLAGS -c src/kernel/console.c -o src/kernel/console.o   # <-- agregado
+$CC $CFLAGS -c src/kernel/kmain.c   -o src/kernel/kmain.o
+$CC $CFLAGS -c src/kernel/kernel.c  -o src/kernel/kernel.o
+$CC $CFLAGS -c src/kernel/console.c -o src/kernel/console.o
+$CC $CFLAGS -c src/kernel/string.c  -o src/kernel/string.o
 
 # Compilar drivers
-$CC $CFLAGS -c src/drivers/fb.c -o src/drivers/fb.o
+$CC $CFLAGS -c src/drivers/fb.c     -o src/drivers/fb.o
 $CC $CFLAGS -c src/drivers/cursor.c -o src/drivers/cursor.o
-$CC $CFLAGS -c src/drivers/mouse.c -o src/drivers/mouse.o
-$CC $CFLAGS -c src/drivers/time.c -o src/drivers/time.o
+$CC $CFLAGS -c src/drivers/mouse.c  -o src/drivers/mouse.o
+$CC $CFLAGS -c src/drivers/time.c   -o src/drivers/time.o
 
 # Enlazar kernel
 $LD $LDFLAGS -o src/kernel.bin src/kernel/*.o src/drivers/*.o
@@ -23,7 +24,7 @@ $LD $LDFLAGS -o src/kernel.bin src/kernel/*.o src/drivers/*.o
 # Crear ISO con GRUB
 mkdir -p src/iso/boot/grub
 cp src/kernel.bin src/iso/boot/kernel.bin
-echo 'set timeout=0' > src/iso/boot/grub/grub.cfg
+echo 'set timeout=0'  > src/iso/boot/grub/grub.cfg
 echo 'set default=0' >> src/iso/boot/grub/grub.cfg
 echo 'menuentry "CultraCore" { multiboot /boot/kernel.bin }' >> src/iso/boot/grub/grub.cfg
 grub-mkrescue -o src/os.iso src/iso
