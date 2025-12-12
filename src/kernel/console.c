c41d7b0bac6b98cdfe4bfec55154f28ed1e83d39
Francisco <frankwither991000@gmail.com>
2025-12-12 13:41:09 -0700

    Update console.c

--- src/kernel/console.c
+++ src/kernel/console.c
@@ -1,9 +1,10 @@
 // src/kernel/console.c
 #include <stdint.h>
+#include "console.h"
 
 volatile char *video = (volatile char*)0xB8000;
 
-void kputs(const char *s) {
+void puts(const char *s) {
     int i = 0;
     while (s[i]) {
         video[i*2] = s[i];
